8a835f03171052c704391bc47ca4766ebf8f85d5
Andres Patiño <andresp199519@gmail.com>
2020-11-20 14:31:48 +0100

    Actualización Alisson

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ document.addEventListener('click', function(event){
     sonido.currentTime = 0;
     if(!sonido) return;
     sonido.play();
-    console.log(boton);
 });
 
-    document.addEventListener('onkeypress', function (event){
-    
 
+    document.addEventListener("keypress", function(event) {
         
+        let key = event.key;
+        if (key = 'l'){
+            document.getElementById('bombo').play();
+        }
+        else return document.getElementById('bombo').muted(true);
+        
+console.log(event)
+
+
     })
+
+
